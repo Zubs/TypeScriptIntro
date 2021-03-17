9b16193bed0bd1518e48fe25de13ad855aade83a
Zubs <zubairidrisaweda@gmail.com>
2021-03-17 11:40:03 +0100

    TypeCasting and DOM

--- public/app.js
+++ public/app.js
@@ -1,59 +1,13 @@
 "use strict";
-// Variables
-var age = 21;
-console.log(age);
-// Types
-var getCircumference = function (diameter) {
-    return diameter * Math.PI;
-};
-console.log(getCircumference(5));
-// Arrays
-var mixed = ["Age", 32, true];
-mixed.push("Hello");
-var numberOnly = [1, 2, 3, 4, 5];
-numberOnly.push(11);
-// Objects
-var zubair = {
-    name: "Zubair Idris Aweda",
-    age: 19,
-    male: true
-};
-// Types
-var students;
-var loggedIn;
-var testCharacter;
-var uuid; // No parentheses if it's not in front of an array.
-var people = [];
-var mixedArray = [];
-var person; // This allows for person to be an array.
-var secondPerson;
-// Any as a type
-var secondAge = 25;
-var secondMixedArray = [];
-// Functions
-var sayHello;
-var addNumbers = function (a, b) {
-    console.log(a + b);
-};
-addNumbers(2, 8);
-// Optional parameters
-// Without default
-var getProduct = function (a, b, c) {
-    if (c) {
-        console.log(a * b * c);
-    }
-    else {
-        console.log(a * b);
-    }
-    ;
-};
-// With default
-var getSum = function (a, b, c) {
-    if (c === void 0) { c = 18; }
-    console.log(a + b + c);
-};
-// Set function return type
-var getDiff = function (a, b) {
-    return a - b;
-};
-var thirdAge = 12;
+// Get Form
+var form = document.querySelector('.form');
+// Get Inputs
+var type = document.querySelector('#type');
+var toOrFrom = document.querySelector('#toOrFrom');
+var details = document.querySelector('#details');
+var amount = document.querySelector('#amount');
+// Add Listener To Form
+form.addEventListener('submit', function (e) {
+    e.preventDefault();
+    console.log(type.value, toOrFrom.value, details.value, amount.valueAsNumber);
+});
--- src/app.ts
+++ src/app.ts
@@ -1,83 +1,15 @@
-// Variables
-const age = 21;
+// Get Form
+const form = document.querySelector('.form') as HTMLFormElement;
 
-console.log(age);
+// Get Inputs
+const type = document.querySelector('#type') as HTMLSelectElement;
+const toOrFrom = document.querySelector('#toOrFrom') as HTMLInputElement;
+const details = document.querySelector('#details') as HTMLTextAreaElement;
+const amount = document.querySelector('#amount') as HTMLInputElement;
 
-// Types
-const getCircumference = (diameter: number) => {
-	return diameter * Math.PI;
-};
+// Add Listener To Form
+form.addEventListener('submit', (e: Event) => {
+	e.preventDefault();
 
-console.log(getCircumference(5));
-
-// Arrays
-let mixed = ["Age", 32, true];
-
-mixed.push("Hello");
-
-let numberOnly = [1, 2, 3, 4, 5];
-
-numberOnly.push(11);
-
-// Objects
-var zubair = {
-	name: "Zubair Idris Aweda",
-	age: 19,
-	male: true
-};
-
-// Types
-let students: number;
-let loggedIn: boolean;
-let testCharacter: string;
-
-let uuid: string | number; // No parentheses if it's not in front of an array.
-
-let people: string [] = [];
-let mixedArray: (string | number | boolean) [] = [];
-
-let person: object; // This allows for person to be an array.
-
-let secondPerson: {
-	name: string,
-	age: number,
-	language: string
-};
-
-// Any as a type
-let secondAge: number = 25;
-let secondMixedArray: any [] = [];
-
-// Functions
-let sayHello: Function;
-
-const addNumbers = (a: number, b: number) => {
-	console.log(a + b);
-};
-
-addNumbers(2, 8);
-
-// Optional parameters
-// Without default
-const getProduct = (a: number, b: number, c?: number) => {
-	if (c) {
-		console.log(a * b * c);
-	} else {
-		console.log(a * b);
-	};
-};
-
-// With default
-const getSum = (a: number, b: number, c: number = 18) => {
-	console.log(a + b + c);
-};
-
-// Set function return type
-const getDiff = (a: number, b: number): number => {
-	return a - b;
-}
-
-// Type Aliases
-type StrOrNum = string | number;
-
-const thirdAge: StrOrNum = 12;
+	console.log(type.value, toOrFrom.value, details.value, amount.valueAsNumber);
+});
