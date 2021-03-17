79562cd94fca7fab9368c18b8f6b98ee25606360
Zubs <zubairidrisaweda@gmail.com>
2021-03-17 14:24:52 +0100

    Some OOP stuff

--- docs/public/app.js
+++ docs/public/app.js
@@ -11,3 +11,19 @@ form.addEventListener('submit', function (e) {
     e.preventDefault();
     console.log(type.value, toOrFrom.value, details.value, amount.valueAsNumber);
 });
+// Create the Invoice class
+var Invoice = /** @class */ (function () {
+    // Map properties to inputs
+    function Invoice(client, details, amount) {
+        this.client = client;
+        this.details = details;
+        this.amount = amount;
+    }
+    Invoice.prototype.format = function () {
+        return this.client + " owes \u20A6" + this.amount + " for " + this.details;
+    };
+    return Invoice;
+}());
+var invOne = new Invoice("Zubair", "Website money", 2500);
+// An array of only invoices
+var invoices = [];
--- docs/src/app.ts
+++ docs/src/app.ts
@@ -13,3 +13,28 @@ form.addEventListener('submit', (e: Event) => {
 
 	console.log(type.value, toOrFrom.value, details.value, amount.valueAsNumber);
 });
+
+// Create the Invoice class
+class Invoice {
+
+	// Declare properties
+	client: string;
+	details: string;
+	amount: number;
+	
+	// Map properties to inputs
+	constructor(client: string, details: string, amount: number) {
+		this.client = client;
+		this.details = details;
+		this.amount = amount;
+	}
+
+	format () {
+		return `${ this.client } owes ₦${ this.amount } for ${ this.details }`;
+	}
+}
+
+const invOne = new Invoice("Zubair", "Website money", 2500);
+
+// An array of only invoices
+let invoices: Invoice [] = [];
